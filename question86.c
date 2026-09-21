29da267b22ac70c4bcf36c7db57659579ba5e074
Aaditya Singh <AADITYA.34660@stu.upes.ac.in>
2026-09-21 22:59:48 +0530

    Add files via upload

---
+++ question86.c
@@ -0,0 +1,44 @@
+//Q86: Check if a string is a palindrome.
+
+/*
+Sample Test Cases:
+Input 1:
+madam
+Output 1:
+Palindrome
+
+Input 2:
+hello
+Output 2:
+Not palindrome
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0, palindrome = 1;
+
+    scanf("%s", str);
+
+    // Find length
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    // Compare characters
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            palindrome = 0;
+            break;
+        }
+    }
+
+    if (palindrome)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
